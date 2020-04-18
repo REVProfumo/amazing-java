a5cc205f122d3f68a97d79e0fcbcb0291996030e
REVProfumo <33388181+REVProfumo@users.noreply.github.com>
2020-04-18 11:30:02 +0200

    multithreading with streams

--- threadsAndGenerics/ThreadsAndGenerics.java
+++ threadsAndGenerics/ThreadsAndGenerics.java
@@ -1,30 +1,39 @@
 package amazingJava;
 
-import java.util.concurrent.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class ThreadsAndGenerics {
     public static void main(String ...args) throws Exception {
-        System.out.println(new Response().getResponse(5));
+        System.out.println(new Response().getResponse(Arrays.asList(5, 7)));
     }
 }
 
 class Response extends GenericClass<String> {
 
-    public String getResponse(Integer i)  throws Exception {
-        return getGenericResponse(() -> getSingleThreadResponse(i));
+    public List<String> getResponse(List<Integer> list)  throws Exception {
+        return getGenericResponse(() -> getSingleThreadResponse(list));
     }
     
-    public String getSingleThreadResponse(Integer i) {
-        return i.toString();
+    public List<String> getSingleThreadResponse(List<Integer> list) {
+        return list.stream().map(i -> i.toString()).collect(Collectors.toList());
     }
 }
 
-class GenericClass <U>  {
+class GenericClass<U>  {
   
-    public U getGenericResponse(Callable<U> funcInterface) throws Exception {
+    public List<U> getGenericResponse(Callable<List<U>> funcInterface) throws Exception {
         ExecutorService ex = Executors.newFixedThreadPool(10);
-        Future<U> future = ex.submit(funcInterface);
-        U res = future.get();
+        Future<List<U>> future = ex.submit(funcInterface);
+        List<U> res = future.get();
+        if(ex != null) {
+          ex.shutdown();
+        }
         return res;
     }
 } 
